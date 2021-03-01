6bc708abf18d15410cc30dc80c1c04e6bd327618
wenqi <hnywolf@gmail.com>
2021-03-01 17:42:31 +0800

    [new]
    dota2 game mode run

---
+++ dota2.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/sh
+#gamemoderun /home/wenqi/.local/share/Steam/steamapps/common/dota\ 2\ beta/game/bin/linuxsteamrt64/dota2 +engine_experimental_drop_frame_ticks 1 +@panorama_min_comp_layer_dimension 0 -prewarm_panorama -perfectworld -novid
+gamemoderun /home/wenqi/.local/share/Steam/steamapps/common/dota\ 2\ beta/game/bin/linuxsteamrt64/dota2 +engine_experimental_drop_frame_ticks 1 +@panorama_min_comp_layer_dimension 0 -prewarm_panorama -novid
+
